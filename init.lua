b120406a015cfa7a756f71600ee2d4e9530a672b
Clayton Ramstedt <clayton.ramstedt@gmail.com>
2024-07-11 10:50:26 -0600

    changing the tab and buffer display behavior

--- init.lua
+++ init.lua
@@ -104,6 +104,9 @@ vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right win
 vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
 vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
 
+vim.keymap.set("n", "<Tab>", ":bnext\n", { desc = "Move to the next buffer" })
+vim.keymap.set("n", "<S-Tab>", ":bprevious\n", { desc = "Move to the last buffer" })
+
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
