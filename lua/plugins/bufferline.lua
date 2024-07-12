edc9e1a9815cba6179824521afb739186907fc4f
Clayton Ramstedt <clayton.ramstedt@gmail.com>
2024-07-12 09:59:11 -0600

    replaced mini.nvim with other plugins

---
+++ lua/plugins/bufferline.lua
@@ -0,0 +1,22 @@
+return {
+	"akinsho/bufferline.nvim",
+	version = "v3.*",
+	dependencies = "nvim-tree/nvim-web-devicons",
+	config = function()
+		require("bufferline").setup({
+			options = {
+				mode = "tabs", -- set to "tabs" to only show tabpages instead
+				numbers = "ordinal",
+				indicator = {
+					style = "underline",
+				},
+				separator_style = "slant",
+				diagnostics = "nvim_lsp",
+				diagnostics_indicator = function(count, level, diagnostics_dict, context)
+					local icon = level:match("error") and " " or " "
+					return " " .. icon .. count
+				end,
+			},
+		})
+	end,
+}
--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -46,4 +46,14 @@ return { -- You can easily change to a different colorscheme.
 			vim.cmd.hi("Comment gui=none")
 		end,
 	},
+	{
+		"sainnhe/everforest",
+		priority = 1000, -- Make sure to load this before all the other start plugins.
+		init = function()
+			-- vim.cmd.colorscheme("kanagawa")
+
+			-- You can configure highlights by doing something like:
+			vim.cmd.hi("Comment gui=none")
+		end,
+	},
 }
