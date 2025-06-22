afb7ef5aa1eed7b1e5cfdf7c92a8fd74547dc2c7
Clayton Ramstedt <claytonramstedt@Mac.localdomain>
2025-06-21 19:01:31 -0600

    added some AI plugins

--- lua/plugins/llama.cpp.lua
+++ lua/plugins/llama.cpp.lua
@@ -1,8 +1,8 @@
 return {
-		'ggml-org/llama.vim',
-		init = function() 
-			vim.g.llama_config = {
-				show_info = 0
-			}
-		end
+	"ggml-org/llama.vim",
+	init = function()
+		vim.g.llama_config = {
+			show_info = 0,
+		}
+	end,
 }
